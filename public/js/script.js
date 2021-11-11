45364d2095139065462397c755f959bee07a8128
Vincent Chu <vwchu@users.noreply.github.com>
2021-11-11 01:33:02 -0500

    Refactor script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,14 @@
 (function () {
   'use strict';
 
-  const GetTaxesAPI     = '/api/taxes';
-  const GetTaxByCodeAPI = '/api/taxes/:code';
-
   /**
-   * Supplant does variable substitution on the string. 
-   * It scans through the string looking for expressions enclosed in 
-   * {{ }} braces. If an expression is found, use it as a key on the object,
-   * and if the key has a string value or number value, it is substituted for
-   * the bracket expression and it repeats. This is useful for automatically
-   * fixing URLs or for templating HTML.
-   *
+   * Supplant does variable substitution on the string. It scans through the
+   * string looking for expressions enclosed in {{ }} braces. If an expression
+   * is found, use it as a key on the object, and if the key has a string value
+   * or number value, it is substituted for the bracket expression and it repeats.
+   * This is useful for automatically fixing URLs or for templating HTML.
    * Based on: http://www.crockford.com/javascript/remedial.html
-   * 
+   *
    * @param {string} str 
    * @param {object} object
    * @returns {string}
@@ -28,69 +23,45 @@
     );
   }
 
-  function doAjax(url, method) {
-    // Create the XHR request
-    const request = new XMLHttpRequest();
-    // Return it as a Promise
-    return new Promise(function (resolve, reject) {
-      // Setup our listener to process compeleted requests
-      request.onreadystatechange = function () {
-        // Only run if the request is complete
-        if (request.readyState !== 4) return;  
-        // Process the response
-        if (request.status >= 200 && request.status < 300) { // When successful
+  /**
+   * Make an AJAX request with XHR. Returns a Promise.
+   * 
+   * @param {string} url 
+   * @param {'GET'|'POST'|'PUT'|'HEAD'|'DELETE'} method 
+   * @param {*} data
+   * @returns {Promise}
+   */
+  function doAjax(url, method, data) {
+    const request = new XMLHttpRequest();                    // create the XHR request    
+    return new Promise(function (resolve, reject) {          // return it as a Promise
+      request.onreadystatechange = function () {             // setup our listener to process compeleted requests
+        if (request.readyState !== 4) return;                // only run if the request is complete
+        if (request.status >= 200 && request.status < 300) { // process the response, when successful
           resolve(JSON.parse(request.responseText));
-        } else { // When failed
+        } else { // when failed
           reject({
             status: request.status,
             statusText: request.statusText
           });
         }
       };
-      // Setup our HTTP request
-		  request.open(method || 'GET', url, true);
-		  // Send the request
-		  request.send();
+      request.open(method || 'GET', url, true);                       // setup our HTTP request
+      if (data) {                                                     // when data is given...
+        request.setRequestHeader("Content-type", "application/json"); // set the request content-type as JSON, and
+        request.send(JSON.stringify(data));                           // send data as JSON in the request body.
+      } else {
+        request.send(); // send the request
+      }
     });
   }
 
-  function changeView(url, title, html) {
-    const data = { url, title, html };
-    document.title = title;
-    document.getElementById('page').innerHTML = html;
-    if (window.location.hash !== url) {
-      window.history.pushState(data, '', url);
-    }
-  }
-
-  window.onpopstate = function (e) {
-    if (e.state) {
-      document.title = e.state.title;
-      showView(e.state.url);
-    }
-  };
-
-  const templates = {};
-  const routes = {
-    provinces() {
-      doAjax(GetTaxesAPI).then((taxes) => {
-        const content = taxes.map(t => supplant(templates['province-item'], t)).join('');
-        const html    = supplant(templates['provinces-page'], { content });
-
-        changeView('#/provinces', 'Provinces', html);
-      });
-    },
-    taxes(code) {
-      doAjax(GetTaxByCodeAPI.replace(':code', code)).then((tax) => {
-        changeView(`#/taxes/${code}`, `Taxes in ${tax.province}`, supplant(templates['tax-page'], tax));
-      });
-    },
-    index() {
-      routes.provinces();
-    }
-  };
-
-  function showView(hash) {
+  /**
+   * Invoke the route associated with the given URL hash. If no route matches,
+   * redirects to the default route.
+   *
+   * @param {string} hash 
+   */
+  function invokeRoute(hash) {
     if (hash.startsWith('#/')) {
       const [ route, ...params ] = hash.substr(2).split('/');
       if (routes[route]) {
@@ -103,10 +74,59 @@
     }
   }
 
-  document.querySelectorAll('script[type="text/x-template"]').forEach((el) => {
-    templates[el.id] = el.innerText;
-  });
+  /**
+   * Actually change the content of the view. Replaces the innerHTML in the
+   * #page element, and then updates the history, either by pushing a new state
+   * or replacing the existing state if this is a redirection.
+   *
+   * @param {string} url 
+   * @param {string} title 
+   * @param {string} html 
+   * @param {boolean} isRedirect
+   */
+  function changeView(url, title, html, isRedirect = false) {
+    const data = { url, title, html };
+    document.title = title;
+    document.getElementById('page').innerHTML = html;
+    if (window.location.hash !== url) {
+      if (isRedirect) {
+        window.history.replaceState(data, '', url);
+      } else {
+        window.history.pushState(data, '', url);
+      }
+    }
+  }
 
-  window.addEventListener('hashchange', () => showView(window.location.hash));
-  showView(window.location.hash);
+  const GetTaxesAPI     = '/api/taxes';
+  const GetTaxByCodeAPI = '/api/taxes/:code';
+
+  const templates = {};
+  const routes = {
+    provinces(isRedirect) {
+      doAjax(GetTaxesAPI).then((taxes) => {
+        const content = taxes.map(t => supplant(templates['province-item'], t)).join('');
+        const html    = supplant(templates['provinces-page'], { content });
+
+        changeView('#/provinces', 'Provinces', html, isRedirect);
+      });
+    },
+    taxes(code) {
+      doAjax(GetTaxByCodeAPI.replace(':code', code)).then((tax) => {
+        changeView(`#/taxes/${code}`, `Taxes in ${tax.province}`, supplant(templates['tax-page'], tax));
+      });
+    },
+    index() {
+      routes.provinces(true);
+    }
+  };
+
+  document.querySelectorAll('script[type="text/x-template"]').forEach((el) => { templates[el.id] = el.innerText; });
+  window.addEventListener('hashchange', () => invokeRoute(window.location.hash));
+  window.addEventListener('popstate', (ev) => {
+    if (ev.state) {
+      document.title = ev.state.title;
+      invokeRoute(ev.state.url);
+    }
+  });
+  invokeRoute(window.location.hash);
 }());
